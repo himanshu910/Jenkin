69a6817b93eeedfe8f4c75c5de3a6dd21d8060fc
himanshu910 <38109511+himanshu910@users.noreply.github.com>
2018-12-14 13:28:06 +0530

    First commit

---
+++ src/test/java/jenkinsDemo/JenkinsDemo.java
@@ -0,0 +1,21 @@
+package jenkinsDemo;
+
+
+
+public class JenkinsDemo {
+	
+	
+	
+	
+	public  void psv() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I have been executed via Jenkins");
+		
+
+	}
+	
+	
+	
+
+}
